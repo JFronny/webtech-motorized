cd0af6ece9899afb53f48f226a6cf0238ce2c76e
JFronny <git@akbvopenfl1.hopto.org>
2025-12-11 21:32:23 +0100

    chore: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [basicSsl(), tsconfigPaths()],
+  plugins: [
+    basicSsl(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      pwaAssets: {},
+      manifest: {
+        name: "WebTech Motorized",
+        short_name: "Motorized",
+      }
+    }),
+  ],
   base: "/webtech-motorized/",
   esbuild: {
     jsxFactory: "JSX.createElement",
