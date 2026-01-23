14e89188a2a428844522233c79f62c6fbf7597fb
JFronny <git@akbvopenfl1.hopto.org>
2026-01-23 15:34:56 +0100

    ci: do not build twice

--- mksubmission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-echo "Creating submission archive: submission.zip"
-git archive --format=zip --prefix=source/ HEAD > submission.zip
-7z a submission.zip README.md
-echo "Building project..."
-pnpm run build
-7z a submission.zip dist
-echo "Submission archive created: submission.zip"
